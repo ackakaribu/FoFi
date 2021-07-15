866d446a83228f3402a066d8b8ccce9fb5227242
Heitor Murara <heitor.murara@avenuecode.com>
2021-07-15 14:59:13 -0300

    Create Project

---
+++ FoFi/FoFiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoFiApp.swift
+//  FoFi
+//
+//  Created by Heitor Murara on 15/07/21.
+//
+
+import SwiftUI
+
+@main
+struct FoFiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
